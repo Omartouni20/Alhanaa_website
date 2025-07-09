346bdc6acc6a89eb107f9789730a43261753dd32
Omartouni20 <saidtonyz26@gmail.com>
2025-07-09 13:36:21 +0300

    admin.v1.1

--- cofing/settings.py
+++ cofing/settings.py
@@ -28,7 +28,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'alhanaawebsite.middleware.VisitorTrackingMiddleware',
+'alhana.middleware.VisitorTrackingMiddleware',
 ]
 
 ROOT_URLCONF = 'cofing.urls'
